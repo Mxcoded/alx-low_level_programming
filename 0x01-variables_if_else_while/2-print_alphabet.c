370c20bf2f495f9ffcc6f6c1e4d79b04529d3f9c
Mxcoded <jaywoncoded@gmail.com>
2022-10-15 10:55:30 +0000

    Like is said just print abc to z in smallcase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main-This program print alphabets
+*Return: always 0 i.e main function run successfully
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	return (0);
+}
