06f15e9ab1e97de5844cab126ad14bf861359831
Mxcoded <jaywoncoded@gmail.com>
2022-10-16 22:12:50 +0000

    I Cheat

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,23 +1,30 @@
 #include <stdio.h>
 
 /**
-* main - prints the numbers from 00 to 99
+* main - prints all possible different combinations of 3 digit
 * Return: Always 0 (success)
 */
 int main(void)
 {
-	int n, m;
+	int n, m, l;
 
-	for (n = 48; n <= 57; n++)
+	for (n = 48; n <= 58; n++)
 	{
-		for (m = 48; m <= 57; m++)
+		for (m = 49; m <= 58; m++)
 		{
-			putchar(n);
-			putchar(m);
-			if (n != 57 || m != 57)
+			for (l = 50; l < 58; l++)
 			{
-				putchar(',');
-				putchar(' ');
+				if (l > m && m > n)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
 			}
 		}
 	}
