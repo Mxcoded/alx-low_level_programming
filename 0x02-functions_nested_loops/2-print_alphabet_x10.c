ad2c791dd30f830e71875f6cfc59a2ef1a62d2b8
Mxcoded <jaywoncoded@gmail.com>
2022-10-18 23:16:42 +0000

    Print alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * print_alphabet_x10 - Print alphabets a-z 10 times
+  *
+  *Return: Always 0.
+  */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
